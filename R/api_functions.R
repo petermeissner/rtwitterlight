80bd4b4ae7a94052cc118b0f202fce0ab959b6ba
Peter Meissner <retep.meissner@gmail.com>
2015-10-08 15:17:01 +0200

    adding key and secret options to all functions for passing them through

--- R/api_functions.R
+++ R/api_functions.R
@@ -1,4 +1,4 @@
-tw_user_timeline <- function(screen_name, max_id="", count=200){
+tw_usr_tmln <- function(screen_name, max_id="", count=200, key, secret){
   param= list(
     screen_name = screen_name,
     count = count
@@ -11,3 +11,34 @@ tw_user_timeline <- function(screen_name, max_id="", count=200){
     param = param
   )
 }
+
+
+tw_user_timeline <- function(screen_name, n=1, key="", secret=""){
+  n_loops <- ceiling(n/199)
+  tweets  <- list()
+  max_id  <- ""
+  for(i in seq_len(n_loops) ){
+    cat(i, "")
+    tweets <-
+      c(
+        tweets[-200],
+        tw_usr_tmln(screen_name, count = 200, max_id = max_id, key, secret)
+      )
+    max_id_new <- tail(list_get(tweets, "id_str"),1)
+    if ( max_id != max_id_new ) {
+      max_id <- max_id_new
+    }else{
+        break()
+    }
+  }
+  return(tweets)
+}
+
+
+
+
+
+
+
+
+
--- R/api_get_post.R
+++ R/api_get_post.R
@@ -1,21 +1,21 @@
-twitter_post <- function(path, param){
+twitter_post <- function(path, param, key="", secret=""){
   # tweet away
   req <-
     httr::POST(
       url  = paste0("https://api.twitter.com/1.1/", path),
       body = param,
-      config(token = twitter_token()))
+      config(token = twitter_token(key, secret)))
   # return
   return(httr::content(req))
 }
 
-twitter_get <- function(path, param){
+twitter_get <- function(path, param, key="", secret=""){
   # tweet away
   req <-
     httr::GET(
       url  = paste0("https://api.twitter.com/1.1/", path),
       query = param,
-      httr::config(token = twitter_token()))
+      httr::config(token = twitter_token(key, secret)))
   # return
   return(httr::content(req))
 }
